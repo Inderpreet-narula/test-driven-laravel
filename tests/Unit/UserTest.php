9b0f5edb3f5e5ab05d5e0e1cd2a79a6d24f25e5b
inderpreet singh <inderpreetsingh@ucreate.co.in>
2019-06-16 17:27:18 +0530

    Created testCreateProduct

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use App\Product;
 use Tests\ParentTestClass;
 
 class UserTest extends ParentTestClass
@@ -32,9 +33,10 @@ class UserTest extends ParentTestClass
         $user = new User;
         $user->name = $this->faker->name;
         $user->email = $this->faker->safeEmail;
-        $user->password = $this->faker->password;
+        $user->password = $this->faker->password(6, 8);
         $user->save();
         $this->assertInstanceOf(User::class, $user);
+        return $user->id;
     }
     
     public function testValidateIncorrectEmail()
@@ -42,7 +44,7 @@ class UserTest extends ParentTestClass
         $user = self::getUser();
         $user->name = $this->faker->name;
         $user->email = $this->faker->word;
-        $user->password = $this->faker->password;
+        $user->password = $this->faker->password(6, 8);
         $user->save();
         $this->assertArrayHasKey('email', $user->getErrors()->toArray());
     }
@@ -53,9 +55,22 @@ class UserTest extends ParentTestClass
         $existing_user = self::getUser();
         $user->name = $existing_user->name;
         $user->email = $this->faker->safeEmail;
-        $user->password = $this->faker->password(10);
+        $user->password = $this->faker->password(10, 10);
         $user->save();
         $this->assertArrayHasKey('name', $user->getErrors()->toArray());
         $this->assertArrayHasKey('password', $user->getErrors()->toArray());
+    }
+    
+    /**
+     * @depends testCreateUser
+     */
+    
+    public function testCreateProduct($user_id)
+    {
+        $product = new Product;
+        $product->name = $this->faker->word;
+        $product->created_by = $user_id;
+        $product->save();
+        $this->assertInstanceOf(Product::class, $product);
     }
 }
